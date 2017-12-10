140a0ca342ae0789a9fa554b8cdd1c41a93b2421
Leebyeongman <ssey0921@cnu.ac.kr>
2017-12-11 01:09:03 +0900

    책 참고해서 구현완료 작동완료git add mm-implicit.c

--- mm-implicit.c
+++ mm-implicit.c
@@ -41,21 +41,36 @@
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(p) (((size_t)(p) + (ALIGNMENT-1)) & ~0x7)
 
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define SIZE_PTR(p) ((size_t*)(((char*)(p)) - SIZE_T_SIZE))
 #define WSIZE 4
 #define DSIZE 8
-#define CHUKSIZE (1 << 12)
-#define OVERHEAD
-
-#define MAX(x,y) ((x) >(y)?(X):(y))
+#define CHUNKSIZE (1 << 12)
+#define OVERHEAD 8
+#define MAX(x,y) ((x) >(y)?(x):(y))
 #define PACK(size, alloc) ((size) | (alloc))
-#define PUT(p, val) (*(size_t*)(p) = (val))
+#define GET(p) (*(unsigned int*)(p))
+#define PUT(p, val) (*(unsigned int*)(p) = (val))
 #define GET_SIZE(p) (GET(p) & ~ 0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
-
 #define HDRP(bp) ((char*)(bp) - WSIZE)
-#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp))-DSIZE)
-#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char*)(bp) -WSIZE))
+#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE))
 #define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
+
+int mm_init(void);
+void *malloc(size_t size);
+void free(void *ptr);
+void *coalesce(void *bp);
+void *realloc(void *oldptr, size_t size);
+void *calloc(size_t nmemb, size_t size);
+static int in_heap(const void *p);
+static int aligned(const void *p);
+static void *extend_heap(size_t words);
+static void place(void *bp, size_t asize);
+static void *find_fit(size_t asize);
+void mm_checkheap(int verbose);
+static char *heap_listp = 0;
 
 /*
  * Initialize: return -1 on error, 0 on success.
@@ -69,7 +84,7 @@ int mm_init(void) {
 	PUT(heap_listp + DSIZE, PACK(OVERHEAD, 1));		// prologue footer
 	PUT(heap_listp + WSIZE + DSIZE, PACK(0, 1));	// epilogue header
 
-	heap_listp = DSIZE;								
+	heap_listp += DSIZE;								
 
 	if((extend_heap(CHUNKSIZE / WSIZE)) == NULL)	// CHUNKSIZE 바이트의 free block 만큼 empty heap을 확장
 													// 생성된 empty heap을 free block 으로 확장
@@ -98,9 +113,10 @@ void *malloc (size_t size) {
 	}
 
 	extendsize = MAX(asize, CHUNKSIZE);
-	if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+	if((bp = extend_heap(extendsize / WSIZE)) == NULL)
 		return NULL;
 	place(bp, asize);
+
 	return bp;
 }
 
@@ -115,7 +131,6 @@ void free (void *ptr) {
 	PUT(FTRP(ptr), PACK(size, 0));
 
 	coalesce(ptr);
-//    if(!ptr) return;
 }
 
 /*
@@ -133,23 +148,23 @@ void *realloc(void *oldptr, size_t size) {
 	/* If oldptr is NULL, then this is just malloc. */
 	if(oldptr == NULL) {
 		return malloc(size);
-	 }
+	}
     
-     newptr = malloc(size);
+    newptr = malloc(size);
     
-	 /* If realloc() fails the original block is left untouched  */
-	 if(!newptr) {
+	/* If realloc() fails the original block is left untouched  */
+	if(!newptr) {
 	 	return 0;
-	 }
+	}
     
-	 /* Copy the old data. */
-	 oldsize = *SIZE_PTR(oldptr);
-     if(size < oldsize) oldsize = size;
-     memcpy(newptr, oldptr, oldsize);
+	/* Copy the old data. */
+	oldsize = *SIZE_PTR(oldptr);
+    if(size < oldsize) oldsize = size;
+    memcpy(newptr, oldptr, oldsize);
     
-	 /* Free the old block. */
-     free(oldptr);
-     return newptr;
+	/* Free the old block. */
+    free(oldptr);
+    return newptr;
 }
 
 /*
@@ -158,15 +173,12 @@ void *realloc(void *oldptr, size_t size) {
  * needed to run the traces.
  */
 void *calloc (size_t nmemb, size_t size) {
-    size_t prev alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-	// 이전 블럭의 할당 여부 0 = NO, 1 = YES
+	size_t bytes = nmemb * size;
+	void *newptr;
 
-	size_t next alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-	// 다음 블럭의 할당 여부 0 = NO, 1 = YES
-
-	size_t size = GET_SIZE(HDRP(bp));
-	// 현재 블럭의 크기
-	return NULL;
+	newptr = malloc(bytes);
+	memset(newptr, 0, bytes);
+	return newptr;
 }
 
 
@@ -190,4 +202,83 @@ static int aligned(const void *p) {
  * mm_checkheap
  */
 void mm_checkheap(int verbose) {
+}
+
+void *coalesce(void *bp){
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))); // 이전 블럭의 할당 여부 0 = NO, 1 = YES
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))); // 다음 블럭의 할당 여부 0 = NO, 1 = YES
+	size_t size = GET_SIZE(HDRP(bp)); 				   // 현재 블럭의 크기
+
+	if(prev_alloc && next_alloc){
+		return bp;
+	}
+	else if(prev_alloc && !next_alloc){
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+	else if(!prev_alloc && next_alloc){
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+	
+		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+
+		bp = PREV_BLKP(bp);
+	}
+	else if(!prev_alloc && next_alloc){
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+	else {
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+	}
+	return bp;
+}
+
+static void *extend_heap(size_t words){
+	char *bp;
+	size_t size;
+
+	size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+	if((long)(bp = mem_sbrk(size)) == -1)
+		return NULL;
+
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+	
+	return coalesce(bp);
+}
+
+static void place(void*bp, size_t asize){
+	size_t csize = GET_SIZE(HDRP(bp));
+
+	if((csize - asize) >= (2*DSIZE)){
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(csize-asize, 0));
+		PUT(FTRP(bp), PACK(csize-asize, 0));
+	}
+	else{
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
+	}
+}
+
+static void *find_fit(size_t asize){
+	void *bp;
+
+	for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+		if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+			return bp;
+		}
+	}
+	return NULL;
 }
